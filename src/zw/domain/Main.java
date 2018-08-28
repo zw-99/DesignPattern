0764fbe4dc7ab1d983bc1c7fc52568da453688a9
zw-99 <zw-99@users.noreply.github.com>
2018-08-28 15:56:50 +0800

    add factory pattern and example

--- src/zw/domain/Main.java
+++ src/zw/domain/Main.java
@@ -1,10 +1,17 @@
 package zw.domain;
 
+import zw.calculator.factory.Operation;
+import zw.calculator.factory.OperationFactory;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		
+		//利用工厂模式设计的面向对象计算器
+		Operation operation = OperationFactory.createOperation("+");
+		operation.setNumberA(2);
+		operation.setNumberB(1);
+		double result = operation.getResult();
 	}
 
 }
